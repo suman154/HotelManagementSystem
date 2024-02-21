5c18dc4240e04a65ed5961ca1eb5b10898c48a71
suman154 <sumanb@gmail.com>
2024-02-21 07:53:54 +0545

    CRUD Operation

--- management/urls.py
+++ management/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import RoomTypeView
+from .views import RoomView
+
 
 urlpatterns = [
     path('room-type/', RoomTypeView.as_view({'get':'list', 'post':'create'})),
+    path('room-type/<int:pk>/',RoomTypeView.as_view({'get':'retrieve', 'put':'update','delete':'destroy'})),
+    path('room/',RoomView.as_view({'get':'list', 'post':'create'}))
 ]
--- management/views.py
+++ management/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .models import RoomType
-from .serializers import RoomTypeSerializer
+from .models import RoomType,Room
+from .serializers import RoomTypeSerializer, RoomSerializer
 
 # Create your views here.
 class RoomTypeView(ModelViewSet):
     queryset = RoomType.objects.all()
     serializer_class = RoomTypeSerializer
+
+
+class RoomView(ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
