75daf42e8b5a22c8b326613950a636a6b454a939
suman154 <sumanb@gmail.com>
2024-02-22 07:28:24 +0545

    HMS

--- management/views.py
+++ management/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import GenericAPIView
 from .models import RoomType,Room
 from .serializers import RoomTypeSerializer, RoomSerializer
+from rest_framework.response import Response
 
 # Create your views here.
 class RoomTypeView(ModelViewSet):
@@ -12,3 +14,9 @@ class RoomTypeView(ModelViewSet):
 class RoomView(ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+def get(self,request):
+    Room_objs = self.get_queryset()
+    serializer = self.serializer_class(Room_objs,many=True)
+    return Response(serializer.data)
