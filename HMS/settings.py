6f5e3e2f54343e723062d76382dd49e0a9a1f3b2
suman154 <sumanb@gmail.com>
2024-03-04 18:38:54 +0545

    Error Solve

--- HMS/settings.py
+++ HMS/settings.py
@@ -123,7 +123,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES' : [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated',
         'core.permissions.CustomDjangoModelPermission'
     ]
 }
